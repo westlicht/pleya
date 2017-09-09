f29a1880c6d547e3fcc4305426b41e53df4561a9
Simon Kallweit <simon.kallweit@gmail.com>
2017-09-10 00:44:22 +0200

    implemented basic player logic

--- sketch/pleya/pleya.ino
+++ sketch/pleya/pleya.ino
@@ -1,8 +1,8 @@
-// Specifically for use with the Adafruit Feather, the pins are pre-set here!
+// pleya - Kids mp3 player
 
-// include SPI, MP3 and SD libraries
 #include <SPI.h>
 #include <SD.h>
+#include <EEPROM.h>
 #include <Adafruit_VS1053.h>
 
 // These are the pins used
@@ -51,42 +51,196 @@
   #define VS1053_DREQ     31     // VS1053 Data request, ideally an Interrupt pin
 #endif
 
+#define DEBUG 1
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
+// Number of playlists (maximum is 9)
+static const int playlistCount = 9;
+
+// Pin connected to a variable voltage divider used for volume control
+static const int volumePin = 18; // A0
+
+// Pins connected to buttons used for playback control
+static const int playlistPins[playlistCount] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+static const int backwardPin = 10;
+static const int forwardPin = 10;
+
+int currentPlaylist = -1;
+int currentTrack = -1;
+bool isPlaying = false;
+
+File currentDir;
+File currentFile;
+
+Adafruit_VS1053_FilePlayer musicPlayer =
   Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
-void setup() {
-  Serial.begin(115200);
+// write playlist/track to EEPROM
+void writeEEPROM(int playlist, int track) {
+#if DEBUG
+  Serial.print("writeEEPROM("); Serial.print(playlist); Serial.print(", "); Serial.print(track); Serial.println(")");
+#endif
+  EEPROM.update(0, 0);
+  EEPROM.update(1, 0xba);
+  EEPROM.update(2, playlist);
+  EEPROM.update(3, track);
+  EEPROM.update(0, 0xab);
+}
 
+// read playlist/track from EEPROM
+bool readEEPROM(int &playlist, int &track) {
+#if DEBUG
+  Serial.println("readEEPROM()");
+#endif
+  if (EEPROM.read(0) != 0xab || EEPROM.read(1) != 0xba) {
+    return false;
+  }
+  playlist = EEPROM.read(2);
+  track = EEPROM.read(3);
+  return true;
+}
+
+// play next track in playlist or the specified track if track != -1
+void playTrack(int playlist, int track = -1) {
+#if DEBUG
+  Serial.print("playTrack("); Serial.print(playlist); Serial.print(", "); Serial.print(track); Serial.println(")");
+#endif
+
+  if (isPlaying) {
+    isPlaying = false;
+    musicPlayer.stopPlaying();
+    while (!musicPlayer.stopped()) {
+      delay(1);
+    }
+  }
+
+  char *path = "/x";
+  path[1] = '0' + playlist;
+
+  if (playlist != currentPlaylist || track != -1) {
+    // open directory
+    if (currentDir) {
+      currentDir.close();
+    }
+    currentPlaylist = playlist;
+    currentTrack = -1;
+    currentDir = SD.open(path);
+    currentDir.rewindDirectory();
+  }
+
+  if (!currentDir) {
+#if DEBUG
+    Serial.print("path '"); Serial.print(path); Serial.println("' does not exist!");
+#endif
+    return;
+  }
+
+  // open next file
+  while (true) {
+    currentFile = currentDir.openNextFile();
+    if (!currentFile) {
+      if (track != -1) {
+        currentDir.rewindDirectory();
+        currentTrack = -1;
+        track = -1;
+        continue;
+      }
+      if (currentTrack == -1) {
+#if DEBUG
+        Serial.print("path '"); Serial.print(path); Serial.println("' does not contain playable files!");
+#endif
+        return;
+      }
+      currentDir.rewindDirectory();
+      currentTrack = -1;
+      continue;
+    }
+    if (currentFile.size() <= 4096) {
+      // skip small files that are artifacts from long file names
+#if DEBUG
+      Serial.print("skipping file '"); Serial.print(currentFile.name()); Serial.println("'");
+#endif
+      currentFile.close();
+      continue;
+    }
+    ++currentTrack;
+    if (track == -1) {
+      break;
+    } else {
+      if (currentTrack == track) {
+        break;      
+      }
+    }
+    currentFile.close();
+  }
+
+#if DEBUG
+  Serial.print("track index "); Serial.println(currentTrack);
+#endif
+
+  writeEEPROM(currentPlaylist, currentTrack);
+
+  char trackName[64];
+  trackName[0] = '0' + playlist;
+  trackName[1] = '/';
+  trackName[2] = '\0';
+  strncat(trackName, currentFile.name(), sizeof(trackName));
+
+  currentFile.close();
+
+  isPlaying = true;
+  if (!musicPlayer.startPlayingFile(trackName)) {
+#if DEBUG
+    Serial.print("failed to start playing "); Serial.println(trackName);
+    isPlaying = false;
+#endif
+  }
+}
+
+// go backward in current track
+void backward() {
+#if DEBUG
+  Serial.println("backward()");
+#endif
+  playTrack(currentPlaylist, currentTrack);
+}
+
+// go forward in current track
+void forward() {
+#if DEBUG
+  Serial.println("forward()");
+#endif
+}
+
+
+void setup() {
   // if you're using Bluefruit or LoRa/RFM Feather, disable the BLE interface
   //pinMode(8, INPUT_PULLUP);
 
-  // Wait for serial port to be opened, remove this line for 'standalone' operation
-//  while (!Serial) { delay(1); }
+#if DEBUG
+  Serial.begin(115200);
+  // Wait for serial port to be opened
+  while (!Serial) {
+    delay(1);
+  }
+  Serial.println("\n\npleya - Kids mp3 player");
+#endif
 
-  Serial.println("\n\nAdafruit VS1053 Feather Test");
-  
-  if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+  if (!musicPlayer.begin()) { 
+#if DEBUG
+     Serial.println("VS1053 not available!");
+#endif
      while (1);
   }
 
-  Serial.println(F("VS1053 found"));
-
-  musicPlayer.setVolume(50,50);
-
-  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+  musicPlayer.setVolume(20, 20);
   
   if (!SD.begin(CARDCS)) {
-    Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
+#if DEBUG
+    Serial.println("SD card not found!");
+#endif
+    musicPlayer.sineTest(0x44, 500);
+    while (1);
   }
-  Serial.println("SD OK!");
-  
-  // list files
-  printDirectory(SD.open("/"), 0);
-  
-  // Set volume for left, right channels. lower numbers == louder volume!
   
 #if defined(__AVR_ATmega32U4__) 
   // Timer interrupts are not suggested, better to use DREQ interrupt!
@@ -99,72 +253,55 @@ void setup() {
   // audio playing
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
 #endif
-  
-  // Play a file in the background, REQUIRES interrupts!
-  Serial.println(F("Playing full track 001"));
-  musicPlayer.playFullFile("track001.mp3");
 
-  Serial.println(F("Playing track 002"));
-  musicPlayer.startPlayingFile("track002.mp3");
+  // play last played track if available or start first playlist
+  int initialPlaylist;
+  int initialTrack;
+  if (readEEPROM(initialPlaylist, initialTrack)) {
+    playTrack(initialPlaylist, initialTrack);
+  } else {
+    playTrack(0);
+  }
 }
 
 void loop() {
-  Serial.print(".");
-  // File is playing in the background
-  if (musicPlayer.stopped()) {
-    Serial.println("Done playing music");
-    while (1) {
-      delay(10);  // we're done! do nothing...
-    }
-  }
   if (Serial.available()) {
     char c = Serial.read();
-    
-    // if we get an 's' on the serial console, stop!
-    if (c == 's') {
-      musicPlayer.stopPlaying();
-    }
-    
-    // if we get an 'p' on the serial console, pause/unpause!
-    if (c == 'p') {
-      if (! musicPlayer.paused()) {
-        Serial.println("Paused");
-        musicPlayer.pausePlaying(true);
-      } else { 
-        Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);
-      }
+    switch (c) {
+      case 'q': backward(); break;
+      case 'w': forward(); break;
+      case '1': playTrack(0); break;
+      case '2': playTrack(1); break;
+      case '3': playTrack(2); break;
+      case '4': playTrack(3); break;
+      case '5': playTrack(4); break;
+      case '6': playTrack(5); break;
+      case '7': playTrack(6); break;
+      case '8': playTrack(7); break;
+      case '9': playTrack(8); break;
+      default: break;
     }
   }
-  delay(100);
+  
+  // handle volume pot
+  int volumeValue = analogRead(volumePin);
+  int volume = volumeValue / 10;
+  volume = volume > 100 ? 100 : volume;
+  volume = 100 - volume;
+  musicPlayer.setVolume(volume, volume);
+  delay(50);
+
+  if (isPlaying && musicPlayer.stopped()) {
+#if DEBUG
+    Serial.println("track done playing");
+#endif
+    playTrack(currentPlaylist);
+  }
 }
 
 
 
-/// File listing helper
-void printDirectory(File dir, int numTabs) {
-   while(true) {
-     
-     File entry =  dir.openNextFile();
-     if (! entry) {
-       // no more files
-       //Serial.println("**nomorefiles**");
-       break;
-     }
-     for (uint8_t i=0; i<numTabs; i++) {
-       Serial.print('\t');
-     }
-     Serial.print(entry.name());
-     if (entry.isDirectory()) {
-       Serial.println("/");
-       printDirectory(entry, numTabs+1);
-     } else {
-       // files have sizes, directories do not
-       Serial.print("\t\t");
-       Serial.println(entry.size(), DEC);
-     }
-     entry.close();
-   }
-}
+
+
 
 
