53edb76e8c03a3b429dacb602940f1d0a6916607
Simon Kallweit <simon.kallweit@gmail.com>
2018-12-27 23:01:41 +0100

    version 0.2
    - allow to configure maximum volume
    - sorting tracks in playlist by alphanumeric order
    - saving playstate (current playlist, track and track position) to SD
    card instead of EEPROM
    - saving current track position every 10 seconds to allow resume after
    power cycle (good for longer story tracks)

--- sketch/pleya/pleya.ino
+++ sketch/pleya/pleya.ino
@@ -1,4 +1,23 @@
 // pleya - Kids mp3 player
+// version 0.2
+// written by Simon Kallweit 2017-2018
+
+// Set this to 1 to enable debug mode
+#define DEBUG 0
+
+// Maximum volume (0-127)
+static const int maxVolume = 127;
+
+// Number of playlists (maximum is 9)
+static const int playlistCount = 6;
+
+// Pin connected to a variable voltage divider used for volume control
+static const int volumePin = 18; // A0
+
+// Pins connected to buttons used for playback control
+static const int playlistPins[playlistCount] = { 13, 21, 19, 12, 22, 20 };
+static const int backwardPin = 11;
+static const int forwardPin = 23;
 
 #include <SPI.h>
 #include <SD.h>
@@ -19,8 +38,25 @@
   #error "Unsupported hardware platform! Use a Adafruit Feather 32u4 instead."
 #endif
 
-#define DEBUG 0
+// Debugging
+#if DEBUG
+  #define dbg_print(_x_) Serial.print(_x_)
+  #define dbg_println(_x_) Serial.println(_x_)
+#else
+  #define dbg_print(_x_)
+  #define dbg_println(_x_)
+#endif
 
+// Copy a string and make sure dst is null-terminated
+void strcopy(char *dst, const char *src, int length) {
+  while (*src != '\0' && length > 1) {
+    *dst++ = *src++;
+    --length;
+  }
+  *dst = '\0';
+}
+
+// Button debouncer
 class Button {
 public:
   void init(int pin) {
@@ -28,82 +64,171 @@ public:
     pinMode(_pin, INPUT_PULLUP);
   }
 
-  bool pressed() {
+  // Returns true if button was pressed
+  bool pressed(bool repeats = false) {
     bool state = !digitalRead(_pin);
-    if (state) {
-      if (++_counter == DEBOUNCE) {
-        return true;
-      } else {
-        _counter = min(DEBOUNCE, _counter);
-      }
+    if (state != _state) {
+      _state = state;
+      _time = millis();
     } else {
-      _counter = 0;
+      if (_debouncedState != _state) {
+        if ((millis() - _time) >= DEBOUNCE) {
+          _debouncedState = _state;
+          if (_debouncedState) {
+            return true;
+          }
+        }
+      }
     }
     return false;
   }
 
 private:
-  static const byte DEBOUNCE = 50;
+  static const int DEBOUNCE = 100;
   int _pin;
-  byte _counter = 0;
+  bool _state = false;
+  bool _debouncedState = false;
+  unsigned long _time;
 };
 
-// Number of playlists (maximum is 9)
-static const int playlistCount = 6;
+// Sorted list of tracks in a playlist
+class TrackList {
+public:
+  int numTracks() const { return _numTracks; }
 
-// Pin connected to a variable voltage divider used for volume control
-static const int volumePin = 18; // A0
+  // Open a playlist and populate sorted track list
+  void open(int playlist) {
+    _playlist = playlist;
+    _numTracks = 0;
 
-// Pins connected to buttons used for playback control
-static const int playlistPins[playlistCount] = { 13, 21, 19, 12, 22, 20 };
-static const int backwardPin = 11;
-static const int forwardPin = 23;
+    char path[3] = { '/', '0' + _playlist, '\0' };
+    File dir = SD.open(path);
+    dir.rewindDirectory();
+    if (dir && dir.isDirectory()) {
+      while (_numTracks < MAXTRACKS) {
+        File file = dir.openNextFile();
+        if (!file) {
+          break;
+        }
+        // Skip directories and small files that are artifacts from long file names
+        if (!file.isDirectory() && file.size() > 4096) {
+          dbg_println(file.name());
+          strcopy(_tracks[_numTracks++], file.name(), MAXFILENAME);
+        }
+        file.close();
+      }
+    }
+    dir.close();
+
+    sort();
+  }
+
+  // Get absolute track filename
+  bool trackFilename(int index, char *filename, int length) {
+    if (index < 0 || index >= _numTracks || length < 3) {
+      return false;
+    }
+    filename[0] = '0' + _playlist;
+    filename[1] = '/';
+    filename[2] = '\0';
+    strncat(filename, _tracks[index], length);
+    return true;
+  }
+
+  void dump() {
+    dbg_println("Tracklist:");
+    dbg_println("------------");
+    for (int i = 0; i < _numTracks; ++i) {
+      dbg_println(_tracks[i]);
+    }
+    dbg_println("------------");
+  }
+
+private:
+  void sort() {
+    for (int i = 1; i < _numTracks; i++) {
+      for (int j = i; j > 0 && strcmp(_tracks[j-1], _tracks[j]) > 0; j--) {
+        char tmp[MAXFILENAME];
+        strcopy(tmp, _tracks[j-1], MAXFILENAME);
+        strcopy(_tracks[j-1], _tracks[j], MAXFILENAME);
+        strcopy(_tracks[j], tmp, MAXFILENAME);
+      }
+    }
+  }
+
+  static const int MAXTRACKS = 50;
+  static const int MAXFILENAME = 13;
+
+  int _playlist = -1;
+  char _tracks[MAXTRACKS][MAXFILENAME];
+  int _numTracks = 0;
+};
 
 // Buttons
 static Button playlistButtons[playlistCount];
 static Button backwardButton;
 static Button forwardButton;
 
+static TrackList trackList;
+
 int currentPlaylist = -1;
 int currentTrack = -1;
 bool isPlaying = false;
 
-File currentDir;
-File currentFile;
+unsigned long lastVolumeUpdate = 0;
+unsigned long lastPlayStateWrite = 0;
 
 Adafruit_VS1053_FilePlayer musicPlayer =
   Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
-// Write playlist/track to EEPROM
-void writeEEPROM(int playlist, int track) {
-#if DEBUG
-  Serial.print("writeEEPROM("); Serial.print(playlist); Serial.print(", "); Serial.print(track); Serial.println(")");
-#endif
-  EEPROM.update(0, 0);
-  EEPROM.update(1, 0xba);
-  EEPROM.update(2, playlist);
-  EEPROM.update(3, track);
-  EEPROM.update(0, 0xab);
-}
+static const char *playStateFilename = "/STATE.DAT";
+static const char playStateHeader[4] = "PLST";
 
-// Read playlist/track from EEPROM
-bool readEEPROM(int &playlist, int &track) {
-#if DEBUG
-  Serial.println("readEEPROM()");
-#endif
-  if (EEPROM.read(0) != 0xab || EEPROM.read(1) != 0xba) {
+// Write playlist/track to SD card
+bool writePlayState(int playlist, int track, unsigned long position) {
+  dbg_print("writePlayState("); dbg_print(playlist); dbg_print(", "); dbg_print(track); dbg_print(", "); dbg_print(position); dbg_println(")");
+
+  File file = SD.open(playStateFilename, FILE_WRITE);
+  if (!file) {
     return false;
   }
-  playlist = EEPROM.read(2);
-  track = EEPROM.read(3);
+
+  file.seek(0);
+  file.write(playStateHeader, sizeof(playStateHeader));
+  file.write((const uint8_t *)(&playlist), sizeof(playlist));
+  file.write((const uint8_t *)(&track), sizeof(track));
+  file.write((const uint8_t *)(&position), sizeof(position));
+  file.close();
+
   return true;
 }
 
-// Play next track in playlist or the specified track if track != -1
-void playTrack(int playlist, int track = -1) {
-#if DEBUG
-  Serial.print("playTrack("); Serial.print(playlist); Serial.print(", "); Serial.print(track); Serial.println(")");
-#endif
+// Read playlist/track from SD card
+bool readPlayState(int &playlist, int &track, unsigned long &position) {
+  dbg_println("readPlayState()");
+
+  File file = SD.open(playStateFilename);
+  if (!file) {
+    return false;
+  }
+
+  char header[sizeof(playStateHeader)];
+  file.read(header, sizeof(header));
+  if (memcmp(header, playStateHeader, sizeof(header)) != 0) {
+    file.close();
+    return false;
+  }
+
+  file.read((uint8_t *)(&playlist), sizeof(playlist));
+  file.read((uint8_t *)(&track), sizeof(track));
+  file.read((uint8_t *)(&position), sizeof(position));
+  file.close();
+
+  return true;
+}
+
+void stopPlaying() {
+  dbg_println("stopPlaying()");
 
   if (isPlaying) {
     isPlaying = false;
@@ -112,106 +237,65 @@ void playTrack(int playlist, int track = -1) {
       delay(1);
     }
   }
+}
 
-  char *path = "/x";
-  path[1] = '0' + playlist;
+// Play next track in playlist or the specified track if track != -1
+void playTrack(int playlist, int track = -1, unsigned long position = 0) {
+  dbg_print("playTrack("); dbg_print(playlist); dbg_print(", "); dbg_print(track); dbg_println(")");
 
-  if (playlist != currentPlaylist || track != -1) {
-    // open directory
-    if (currentDir) {
-      currentDir.close();
-    }
+  stopPlaying();
+
+  if (playlist != currentPlaylist) {
+    trackList.open(playlist);
+    trackList.dump();
     currentPlaylist = playlist;
     currentTrack = -1;
-    currentDir = SD.open(path);
-    currentDir.rewindDirectory();
   }
 
-  if (!currentDir) {
-#if DEBUG
-    Serial.print("path '"); Serial.print(path); Serial.println("' does not exist!");
-#endif
+  if (track >= 0 && track < trackList.numTracks()) {
+    currentTrack = track;
+  } else if (trackList.numTracks() > 0) {
+    currentTrack = (currentTrack + 1) % trackList.numTracks();
+  } else {
+    currentTrack = -1;
+  }
+
+  if (currentTrack == -1) {
     return;
   }
 
-  // Open next file
-  while (true) {
-    currentFile = currentDir.openNextFile();
-    if (!currentFile) {
-      if (track != -1) {
-        currentDir.rewindDirectory();
-        currentTrack = -1;
-        track = -1;
-        continue;
-      }
-      if (currentTrack == -1) {
-#if DEBUG
-        Serial.print("path '"); Serial.print(path); Serial.println("' does not contain playable files!");
-#endif
-        return;
-      }
-      currentDir.rewindDirectory();
-      currentTrack = -1;
-      continue;
-    }
-    if (currentFile.size() <= 4096) {
-      // Skip small files that are artifacts from long file names
-#if DEBUG
-      Serial.print("skipping file '"); Serial.print(currentFile.name()); Serial.println("'");
-#endif
-      currentFile.close();
-      continue;
-    }
-    ++currentTrack;
-    if (track == -1) {
-      break;
-    } else {
-      if (currentTrack == track) {
-        break;      
-      }
-    }
-    currentFile.close();
-  }
-
-#if DEBUG
-  Serial.print("track index "); Serial.println(currentTrack);
-#endif
-
-  writeEEPROM(currentPlaylist, currentTrack);
-
   char trackName[64];
-  trackName[0] = '0' + playlist;
-  trackName[1] = '/';
-  trackName[2] = '\0';
-  strncat(trackName, currentFile.name(), sizeof(trackName));
+  trackList.trackFilename(currentTrack, trackName, sizeof(trackName));
 
-  currentFile.close();
+  dbg_print("track index "); dbg_println(currentTrack);
+  dbg_print("track filename "); dbg_println(trackName);
 
   isPlaying = true;
   if (!musicPlayer.startPlayingFile(trackName)) {
-#if DEBUG
-    Serial.print("failed to start playing "); Serial.println(trackName);
+    dbg_print("failed to start playing "); dbg_println(trackName);
     isPlaying = false;
-#endif
   }
+  if (isPlaying && position > 0) {
+    musicPlayer.fileSeek(position);
+  }
+
+  writePlayState(currentPlaylist, currentTrack, position);
 }
 
 // Go backward in current track
 void backward() {
-#if DEBUG
-  Serial.println("backward()");
-#endif
+  dbg_println("backward()");
+
   playTrack(currentPlaylist, currentTrack);
 }
 
 // Go forward in current track
 void forward() {
-#if DEBUG
-  Serial.println("forward()");
-  Serial.print("file size: "); Serial.println(musicPlayer.fileSize());
-  Serial.print("file position: "); Serial.println(musicPlayer.filePosition());
-  Serial.print("file progress: "); Serial.println((100L * musicPlayer.filePosition()) / musicPlayer.fileSize());
-#endif
+  dbg_println("forward()");
+  dbg_print("file size: "); dbg_println(musicPlayer.fileSize());
+  dbg_print("file position: "); dbg_println(musicPlayer.filePosition());
+  dbg_print("file progress: "); dbg_println((100L * musicPlayer.filePosition()) / musicPlayer.fileSize());
+
   musicPlayer.fileSeek(musicPlayer.filePosition() + (1024L * 256L));
 }
 
@@ -247,32 +331,31 @@ void handleButtons() {
   }
   if (forwardButton.pressed()) {
     forward();
-  }  
+  }
 }
 
-static int volumeFiltered = 0;
+static long volumeFiltered = 0;
 
 void handleVolume() {
   // Read volume pot (0..1023)
-  int volumeValue = analogRead(volumePin);
-  // Map to 0..100
-  int volume = volumeValue / 10;
-  volume = volume > 100 ? 100 : volume;
-  volume = 100 - volume;
+  long volumeValue = analogRead(volumePin);
+  // Map to 0..maxVolume
+  long volume = (volumeValue * maxVolume) / 1000;
+  volume = volume > maxVolume ? maxVolume : volume;
+  volume = 127 - volume;
   // Lowpass filter to prevent volume jittering
-  volumeFiltered = (volumeFiltered * 10 + volume) / 11;
-  // Set volume
-  musicPlayer.setVolume(volumeFiltered, volumeFiltered);
+  volumeFiltered = (volumeFiltered * 9 + volume) / 10;
+  // Set player volume, turn off when really low
+  int volumePlayer = volumeFiltered >= 117 ? 255 : volumeFiltered;
+  musicPlayer.setVolume(volumePlayer, volumePlayer);
 }
 
 void handleNextTrack() {
   // Go to next track if current track finished
   if (isPlaying && musicPlayer.stopped()) {
-#if DEBUG
-    Serial.println("track done playing");
-#endif
+    dbg_println("track done playing");
     playTrack(currentPlaylist);
-  }  
+  }
 }
 
 void setup() {
@@ -289,35 +372,28 @@ void setup() {
   while (!Serial) {
     delay(1);
   }
-  Serial.println("\n\npleya - Kids mp3 player");
 #endif
+  dbg_println("\n\npleya - Kids mp3 player");
 
-  if (!musicPlayer.begin()) { 
-#if DEBUG
-     Serial.println("VS1053 not available!");
-#endif
+  if (!musicPlayer.begin()) {
+     dbg_println("VS1053 not available!");
      while (1);
   }
 
   musicPlayer.setVolume(20, 20);
-  
+
   if (!SD.begin(CARDCS)) {
-#if DEBUG
-    Serial.println("SD card not found!");
-#endif
+    dbg_println("SD card not found!");
     musicPlayer.sineTest(0x44, 500);
     while (1);
   }
-  
-  // Timer interrupts are not suggested, better to use DREQ interrupt!
-  // but we don't have them on the 32u4 feather...
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
 
   // Play last played track if available or start first playlist
   int initialPlaylist;
   int initialTrack;
-  if (readEEPROM(initialPlaylist, initialTrack)) {
-    playTrack(initialPlaylist, initialTrack);
+  unsigned long initialPosition;
+  if (readPlayState(initialPlaylist, initialTrack, initialPosition)) {
+    playTrack(initialPlaylist, initialTrack, initialPosition);
   } else {
     playTrack(0);
   }
@@ -328,18 +404,28 @@ void loop() {
   handleSerial();
 #endif
   handleButtons();
-  
-  static int counter;
-  if (++counter % 100 == 0) {
+
+  unsigned long currentTime = millis();
+
+  // Update volume
+  if (currentTime - lastVolumeUpdate > 10) {
+    lastVolumeUpdate = currentTime;
     handleVolume();
+  }
+
+  // Feed data to the music player
+  if (musicPlayer.readyForData()) {
+    musicPlayer.feedBuffer();
+    delay(5);
+    // Write state every 10 seconds but just after filling play buffer to avoid underrun
+    if (currentTime - lastPlayStateWrite > 10000) {
+      lastPlayStateWrite = currentTime;
+      if (isPlaying) {
+        writePlayState(currentPlaylist, currentTrack, musicPlayer.filePosition());
+      }
+    }
   }
 
   handleNextTrack();
 }
-
-
-
-
-
-
 
